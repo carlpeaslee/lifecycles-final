7d8d1c0a7dd903d20f6834ceae30524cfc8f9b0e
Carl Peaslee <cpeaslee@gmail.com>
2017-06-17 10:26:30 -0500

    show how the logger works with an example

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react'
+import {LoggerExample} from './loggerExample'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h2>Hello!</h2>
+        <LoggerExample/>
       </div>
     )
   }
 }
 
 
-function myTestWrapper(WrappedComponent){
-  return class extends Component {
-    render() {
-      return (
-        <div
-          style={{
-            backgroundColor: "blue"
-          }}
-        >
-          <WrappedComponent/>
-        </div>
-      )
-    }
-  }
-}
-
-App = myTestWrapper(App)
+// function myTestWrapper(WrappedComponent){
+//   return class extends Component {
+//     render() {
+//       return (
+//         <div
+//           style={{
+//             backgroundColor: "blue"
+//           }}
+//         >
+//           <WrappedComponent/>
+//         </div>
+//       )
+//     }
+//   }
+// }
+//
+// App = myTestWrapper(App)
 
 export default App
